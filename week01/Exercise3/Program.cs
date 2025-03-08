64e3aff881ff7f1608a18534c03652e0d0778f71
joelsama <codeddraxon99@gmail.com>
2025-03-08 22:31:26 +0100

    number game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random random = new Random();
+        string playAgain;
+
+        do
+        {
+            int magicNumber = random.Next(1, 101);
+            int guess;
+            int attempts =0;
+
+            Console.WriteLine("I have picked a number between 1 and 100. Try to guess it!");
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {attempts} attempts.");
+                }
+            } while (guess != magicNumber);
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
